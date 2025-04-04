8ef63112cf3692c1d611c9985fb16735b8b917ee
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-04-04 17:18:20 +0300

    Some test cases were executed to verify the functionality and outputs of the added classes.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import model.Author;
+import model.Book;
+import model.Person;
+import model.Reader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person mustafa = new Reader("Mustafa");
+
+
+        Book book1 = new Book("Java Programming", new Author("John Doe"), 7);
+        Book book2 = new Book("Design Patterns", new Author("Ali Veli"), 5);
+
+        System.out.println( book1.getStatus());
+        Reader reader = (Reader) mustafa;
+       reader.barrowBook(book1);
+
+
     }
 }
