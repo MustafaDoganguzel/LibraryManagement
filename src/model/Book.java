c80551ead45a56d731f115ef4a6e3ba92b6e3178
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-04-04 17:16:56 +0300

    Implement Book class with attributes and status management
    Implemented the Book class with attributes: bookId, name, author, price, status, edition, and dateOfPurchase.
    
    Used a static counter to assign unique bookIds.
    
    Initialized status as AVAILABLE and dateOfPurchase as the current date.
    
    Overrode equals() and hashCode() for object comparison based on bookId, author, and name.
    
    Implemented display() to print book details.
    
    Added setStatus(Status newStatus) method with validation to prevent invalid or redundant status changes.
    
    Overrode toString() for better object representation.

---
+++ src/model/Book.java
@@ -0,0 +1,95 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Book {
+    private static int bookIdCounter = 1;
+    private int bookId;
+    private Author author;
+    private String name;
+    private int price;
+    private Status status;
+    private String edition;
+    private LocalDate dateOfPurchase;
+
+    public Book( String name, Author author,  int price){
+        this.bookId = bookIdCounter++;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.status = Status.AVAILABLE;
+        this.dateOfPurchase = LocalDate.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(bookId, book.bookId) && Objects.equals(author, book.author) && Objects.equals(name, book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookId, author, name);
+    }
+
+    public void display(){
+        System.out.println( "Book id :" + bookId
+        + " | Author: " +getAuthor()
+        + " | Price: " + getPrice()
+        + " | Date of Purchase " + getDateOfPurchase());
+
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status newStatus){
+        if(newStatus == null ){
+            throw new IllegalArgumentException("Please enter valid status");
+        }
+        if(status == newStatus){
+            throw new IllegalArgumentException("Status is same!");
+        }
+        this.status = newStatus;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public LocalDate getDateOfPurchase() {
+        return dateOfPurchase;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", author=" + author +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", status='" + status + '\'' +
+                ", edition='" + edition + '\'' +
+                ", dateOfPurchase='" + dateOfPurchase + '\'' +
+                '}';
+    }
+}
