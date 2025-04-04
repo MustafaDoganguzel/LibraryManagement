c331c0ccb8246f7b0a8099611adaaf6debaad36f
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-04-04 17:14:47 +0300

    Implement Author Class
    The Author class extends the Person class.
    
    A Set<Book> is used to store the books written by the author.
    
    Added the newBook(Book book) method to add a new book.
    
    Implemented showBook() to display all books.
    
    Overrode the whoYouAre() method to define the author.
    
    Overrode the toString() method.

---
+++ src/model/Author.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Author extends Person{
+
+    private Set<Book> books = new HashSet<>();
+//    private Map<Book, String> books = new HashMap<>();
+
+    public Author(String name) {
+        super(name);
+    }
+
+    @Override
+    public void whoYouAre(){
+        System.out.println("Author" + getName());
+    }
+
+    public boolean newBook (Book book){
+        if (book == null) {
+            throw new IllegalArgumentException("Eklemek istenen kitap bos olamaz");
+        }
+        return books.add(book);
+
+    }
+
+    public void showBook (){
+        System.out.println("All Books: ");
+        for (Book book : books){
+            book.display();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Author" + getName();
+    }
+}
