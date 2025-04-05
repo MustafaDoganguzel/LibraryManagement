2ee20f30f707100c4547903d1bdfeaa024d7cee3
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-04-05 17:56:53 +0300

    Implement StudyBooks Class

---
+++ src/model/book/StudyBooks.java
@@ -0,0 +1,9 @@
+package model.book;
+
+import model.person.Author;
+
+public class StudyBooks extends Book {
+    public StudyBooks(String name, Author author, int price) {
+        super(name, author, price);
+    }
+}
