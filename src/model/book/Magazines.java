a8f76edb3ab59c686c7aa7c76b55bace4e3b0b91
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-04-05 17:56:42 +0300

    Implement Magazines Class

---
+++ src/model/book/Magazines.java
@@ -0,0 +1,10 @@
+package model.book;
+
+import model.person.Author;
+
+public class Magazines extends Book {
+
+    public Magazines(String name, Author author, int price) {
+        super(name, author, price);
+    }
+}
