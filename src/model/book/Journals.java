cf83403a66ebafc11075cd131f7b8497db39d600
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-04-05 17:56:32 +0300

    Implement Journals Class

---
+++ src/model/book/Journals.java
@@ -0,0 +1,9 @@
+package model.book;
+
+import model.person.Author;
+
+public class Journals extends Book{
+    public Journals(String name, Author author, int price) {
+        super(name, author, price);
+    }
+}
