3c3882974aea0a47283e058d83763e915c01c9d6
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-04-10 02:03:35 +0300

    Fix StudyBooks class constructor: Added missing closing parenthesis and corrected constructor overload.

--- src/model/book/StudyBooks.java
+++ src/model/book/StudyBooks.java
@@ -3,7 +3,7 @@ package model.book;
 import model.person.Author;
 
 public class StudyBooks extends Book {
-    public StudyBooks(String name, Author author, int price) {
-        super(name, author, price);
+    public StudyBooks(String name, Author author) {
+        super(name, author);
     }
 }
