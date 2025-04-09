2f56520add84360edeb8f6ece81998a806dd007f
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-04-10 02:03:08 +0300

    Fix Journals class constructor: Added missing closing parenthesis and corrected constructor overload

--- src/model/book/Journals.java
+++ src/model/book/Journals.java
@@ -3,7 +3,7 @@ package model.book;
 import model.person.Author;
 
 public class Journals extends Book{
-    public Journals(String name, Author author, int price) {
-        super(name, author, price);
+    public Journals(String name, Author author) {
+        super(name, author);
     }
 }
